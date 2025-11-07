c77788f25996c10898dbd420bff972636786ebe8
Juan Canepa <macgunmanmc@gmail.com>
2025-11-07 12:31:40 -0300

    feat: add login page and environment variables

---
+++ client/src/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh' 
+    }}>
+      <h1>Página de Login</h1>
+    </div>
+  );
+};
+
+export default Login;
